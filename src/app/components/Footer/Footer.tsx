2d5acedaae7fe69adac608c34b8860eb7d9ee289
Davi Vila <viladavi6@gmail.com>
2024-07-22 16:53:41 -0300

    better footer

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -15,6 +15,11 @@ export default function Footer() {
                         <ul>
                             <Link href="/terms">Termos de Uso</Link>
                         </ul>
+                        <hr className={styles.horizontalLine} />
+                        <p className={styles.text}>
+                            © 2024, Take Control, Inc. Todos os direitos reservados. Take Control, o logotipo da Take Control, e todos os nomes de produtos associados são marcas comerciais ou registradas da Take Control, Inc. nos Estados Unidos da América e em outros lugares. Outras marcas e nomes de produtos são marcas registradas de seus respectivos donos.
+                            Nossos sites podem conter links para outros sites e recursos fornecidos por terceiros. Esses links são fornecidos apenas para a sua conveniência. A Take Control não tem controle sobre o conteúdo desses sites ou recursos e não aceita nenhuma responsabilidade por eles ou por qualquer perda ou dano que possa resultar de seu uso.
+                        </p>
                     </Col>
                 </Row>
             </Container>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,6 @@ const Home = () => {
       <Search />
       <main>
         <Card />
-      </main>
-      <main>
         <Catalog />
       </main>
     </>
